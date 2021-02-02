0f18210ff89c07b14e98710febf07c94a5c92294
tiagofgon <tiago.fg@outlook.com>
2021-02-02 14:48:34 +0000

    .

--- chapter3/build/CMakeFiles/chapter3.section2.dir/cmake_clean.cmake
+++
@@ -1,5 +0,0 @@
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/chapter3.section2.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- chapter3/build/section3/CMakeFiles/chapter3.section3.hpx_queue_template.dir/cmake_clean.cmake
+++
@@ -1,5 +0,0 @@
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/chapter3.section3.hpx_queue_template.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
